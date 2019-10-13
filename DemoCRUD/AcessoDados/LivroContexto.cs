121b4c8287a602b4bc89fa4b68a062a4303e8170
Joelyson David <joelyson.davi@gmail.com>
2019-10-12 23:17:18 -0300

    Projeto ASP.NET MVC5 - Tela de Livros e Generos

---
+++ DemoCRUD/AcessoDados/LivroContexto.cs
@@ -0,0 +1,25 @@
+﻿using DemoCRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace DemoCRUD.AcessoDados
+{
+    public class LivroContexto : DbContext
+    {
+        public DbSet<Genero> Generos { get; set; }
+        public DbSet<Livro> Livros { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // removendo a pluralização automatica do entity
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            // configurando para que todos os campos 'string' tenham tamanho maximo de 100 caracteres
+            modelBuilder.Properties<string>().Configure(c => c.HasMaxLength(100));
+        }
+    }
+}
---
+++ DemoCRUD/Controllers/LivrosController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DemoCRUD.AcessoDados;
+using DemoCRUD.Models;
+
+namespace DemoCRUD.Controllers
+{
+    public class LivrosController : Controller
+    {
+        private LivroContexto db = new LivroContexto();
+
+        // GET: Livros
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult Listar(int pag = 1, int regis = 5) // pagina e quantidade de registro por pag
+        {
+            var livros = db.Livros.Include(l => l.Genero);
+            // Skip((pag-1)*regis): pagina - 1 x registros por pagina
+            // dessa forma toda a cada pagina pularemos os registros que já estão na pagina anterior
+            var livrosPaginados = livros.OrderBy(l => l.Titulo).Skip((pag-1)*regis).Take(regis);
+
+            // como foi mudado o nome da view criada é necessário passar como parametro aqui
+            return PartialView("_Listar", livrosPaginados.ToList());
+        }
+
+        // GET: Livros/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Livro livro = db.Livros.Find(id);
+            if (livro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(livro);
+        }
+
+        // GET: Livros/Create
+        public ActionResult Create()
+        {
+            ViewBag.GeneroId = new SelectList(db.Generos, "Id", "Nome");
+            return View();
+        }
+
+        // POST: Livros/Create
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Titulo,Autor,AnoEdicao,Valor,GeneroId")] Livro livro)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Livros.Add(livro);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.GeneroId = new SelectList(db.Generos, "Id", "Nome", livro.GeneroId);
+            return View(livro);
+        }
+
+        // GET: Livros/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Livro livro = db.Livros.Find(id);
+            if (livro == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.GeneroId = new SelectList(db.Generos, "Id", "Nome", livro.GeneroId);
+            return View(livro);
+        }
+
+        // POST: Livros/Edit/5
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Titulo,Autor,AnoEdicao,Valor,GeneroId")] Livro livro)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(livro).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.GeneroId = new SelectList(db.Generos, "Id", "Nome", livro.GeneroId);
+            return View(livro);
+        }
+
+        // GET: Livros/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Livro livro = db.Livros.Find(id);
+            if (livro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(livro);
+        }
+
+        // POST: Livros/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Livro livro = db.Livros.Find(id);
+            db.Livros.Remove(livro);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
